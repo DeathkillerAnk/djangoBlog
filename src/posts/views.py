933281125e188296a9e121ada0e7b8e296788d94
deathkillerank <aniket.g1999@gmail.com>
2019-02-03 19:37:03 +0530

    url filter

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
-
+from urllib import quote_plus
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
@@ -31,9 +31,11 @@ def post_create(request):
 
 def post_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    share_string = quote_plus(instance.content)
     context = {
         "title" : instance.title,
         "instance" : instance,
+        "share_string" : share_string,
     }
     return render(request,"post_detail.html", context)
 
@@ -54,8 +56,8 @@ def post_list(request):
     }
     return render(request, "post_list.html",context_data)
 
-def post_update(request, id = None):
-    instance = get_object_or_404(Post, id = id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
